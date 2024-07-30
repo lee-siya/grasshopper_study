c98a1ba92726ea8587ef719abba1ad8001be3dd7
lee-siya <seewhy1918@gmail.com>
2024-07-30 18:10:23 +0900

    updated: 원소들의_곱과_합.py

--- lee-siya/프로그래머스_Lv0/원소들의_곱과_합.py
+++ lee-siya/프로그래머스_Lv0/원소들의_곱과_합.py
@@ -1,6 +1,9 @@
-'''정수가 담긴 리스트 num_list 가 주어질 때,
-    모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을
-    return 하도록 solution 함수를 완성해주세요.'''
+# https://school.programmers.co.kr/learn/courses/30/lessons/181929
+
+'''
+정수가 담긴 리스트 num_list 가 주어질 때,
+모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을
+return 하도록 solution 함수를 완성해주세요.'''
 
 
 def solution(num_list):
