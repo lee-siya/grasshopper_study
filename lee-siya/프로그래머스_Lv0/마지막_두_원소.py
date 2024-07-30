c3954ceac27bff7de741dc04d161141c087a3346
lee-siya <seewhy1918@gmail.com>
2024-07-30 18:06:57 +0900

    updated: 마지막_두_원소.py

--- lee-siya/프로그래머스_Lv0/마지막_두_원소.py
+++ lee-siya/프로그래머스_Lv0/마지막_두_원소.py
@@ -1,7 +1,11 @@
-'''정수 리스트 num_list 가 주어질 때,
-    마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을
-    마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여
-    return 하도록 solution 함수를 완성해주세요.'''
+# https://school.programmers.co.kr/learn/courses/30/lessons/181927
+
+'''
+정수 리스트 num_list 가 주어질 때,
+마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을
+마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여
+return 하도록 solution 함수를 완성해주세요.
+'''
 
 
 def solution(num_list):
